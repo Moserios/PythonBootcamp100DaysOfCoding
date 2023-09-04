42a00af6ee7fba551e80b63b98b55eef5b4b92d5
Moser <moserturbo@gmail.com>
2023-09-04 14:13:24 +0200

    Day 3:
    Treasure island
    Love calculator
    Order a pizza

--- day_003_main_conditions_questgame.py
+++ day_003_main_conditions_questgame.py
@@ -13,19 +13,22 @@
 #     elif age < 18:
 #         print("The ticket for teens is $7.0")
 #         bill = 7
+#     elif age > 45 and age < 55:
+#         print("The ticket for teens is $0")
+#         bill = 0
 #     else:
 #         print("The ticket for adults is $12.0")
 #         bill = 12
 #
 #     photo = input("Do you want a photo (Y or N)?\n")
-#     if photo == 'Y':
+#     if photo in ('Y','y'):
 #         bill += 3
 #     print(f"The final price for ticket is ${bill}")
 #
 # else:
 #     print("Sorry, You can't ride the rollercoster!")
 
-
+#######################################
 
 
 ############# check odd or even number
@@ -93,36 +96,169 @@
 # else:
 #     print("Not leap year.")
 
+##################################
+
 
 ############ order a pizza
-print("Welcome to Online PIZZA!")
-bill = 0
-pizza_size = input("What size of pizza do you want (S, M, L)?):\n")
+# while True:
+#     # 🚨 Don't change the code below 👇
+#     print("Welcome to Python Pizza Deliveries!")
+#     size = input("What size pizza do you want? S, M, or L ")
+#     add_pepperoni = input("Do you want pepperoni? Y or N ")
+#     extra_cheese = input("Do you want extra cheese? Y or N ")
+#     # 🚨 Don't change the code above 👆
+#
+#     #Write your code below this line 👇
+#     bill = 0
+#
+#     if size in ('S','s'):
+#         bill += 15
+#         if add_pepperoni in ('Y','y'):
+#             bill += 2
+#
+#
+#     elif size in ('M','m'):
+#         bill += 20
+#         if add_pepperoni in ('Y','y'):
+#             bill += 3
+#
+#
+#     elif size in ('L','l'):
+#         bill += 25
+#         if add_pepperoni in ('Y','y'):
+#             bill += 3
+#
+#
+#     if extra_cheese in ('Y','y'):
+#             bill += 1
+#
+#
+#     print(f"Your final bill is: ${bill}.")
+#     print(' ')
 
-if pizza_size == "S" or "s":
-    print("Small pizza is $15!")
-    bill = 15
-elif pizza_size == "M" or "m":
-    print("Medium pizza is $20!")
-    bill = 20
-elif pizza_size == "L" or "l":
-    print("Large pizza is $25!")
-    bill = 25
+############ ############ ############
+
+############ LOVE CALCULATOR ############
+# while True:
+#     # 🚨 Don't change the code below 👇
+#     print("Welcome to the Love Calculator!")
+#     name1 = input("What is your name? \n")
+#     name2 = input("What is their name? \n")
+#     # 🚨 Don't change the code above 👆
+#
+#     #Write your code below this line 👇
+#     combined_names = name1 + name2
+#     combined_names_lower = combined_names.lower()
+#
+#     calculate_t = combined_names_lower.count("t")
+#     calculate_r = combined_names_lower.count("r")
+#     calculate_u = combined_names_lower.count("u")
+#     calculate_e = combined_names_lower.count("e")
+#     first_number = calculate_t + calculate_r + calculate_u + calculate_e
+#
+#
+#     calculate_l = combined_names_lower.count("l")
+#     calculate_o = combined_names_lower.count("o")
+#     calculate_v = combined_names_lower.count("v")
+#
+#     second_number = calculate_l + calculate_o + calculate_v + calculate_e
+#
+#     combined = int(str(first_number) + str(second_number))
+#
+#     if combined < 10 or combined > 90:
+#         print(f"Your score is {combined}, you go together like coke and mentos.")
+#     elif combined >= 40 and combined <= 50:
+#         print(f"Your score is {combined}, you are alright together.")
+#     else:
+#         print(f"Your score is {combined}.")
+#
+#     print(" ")
+########################
 
 
-    elif age < 18:
-        print("The ticket for teens is $7.0")
-        bill = 7
-    else:
-        print("The ticket for adults is $12.0")
-        bill = 12
+######################## TREASURE ISLAND ########################
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
 
 
-    pepperoni = input("Do you want to add pepperoni (Y or N)?\n")
-    if pepperoni == 'Y':
-    if pizza_size == "S" or "s":
-        bill += 3
-    print(f"Your bill is ${bill}")
+first_step = input('''You are at the road and came across a crossroad. 
+You have only two diredctions: left and right. 
+Where are you going? 
+(L(left) or R(right))?''').lower()
+print("")
 
+if first_step in ('L','l','left'):
+    print('''You turned left and continue journey for a few hours. 
+    Finally you see the road goes to a enormous lake with an island in the middle at a long distance.''')
+    print("")
 else:
-    print("Sorry, You can't ride the rollercoster!")
+    print('''You turned right and in some time the road entered a dark forest. 
+    You've decided to enter it and continue journey. Suddenly a band of bandits jumped at the road and attacked you. 
+    You were one agains seven and you managed to kill three of them but was wounded and killed.''')
+    print("")
+    print("Game Over")
+
+
+if first_step in ('L','l','left'):
+    second_step = input('''You came to the lake shore. 
+    You may wait for a boat or try to swim. 
+    Where are you going to do? 
+    (W(wait) or S(swim)?''').lower()
+    print("")
+    if second_step in ('W','w','wait'):
+        print('''You waited for a days and finally the boat appeared.
+              You entered the boat and sailed to the island in the middle of the huge lake.''')
+        print("")
+    else:
+        print('''You tried to swim to the island, but all your weapons and armour pulled you to the bottom of the lake and you drowned.''')
+        print("")
+        print("Game Over")
+
+if first_step in ('L','l','left') and second_step in ('W','w', 'wait'):
+    third_step = input('''You entered a dungeon and passed long tunnel which ended in a hall with three doors: red, yellow, blue. 
+    Which one you want to open? 
+    (R(red), Y(yellow) or B(blue)?''').lower()
+    print("")
+    if third_step in ('Y','y','yellow'):
+        print("You opened a yellow door and see a big chest with a lot of gold coins and gems in and near it.")
+        print("Congratulations! You Win")
+        print('''
+        *******************************************************************************
+                  |                   |                  |                     |
+         _________|________________.=""_;=.______________|_____________________|_______
+        |                   |  ,-"_,=""     `"=.|                  |
+        |___________________|__"=._o`"-._        `"=.______________|___________________
+                  |                `"=._o`"=._      _`"=._                     |
+         _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+        |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+        |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+                  |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+         _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+        |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+        |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+        ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+        /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+        ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+        /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+        ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+        /______/______/______/______/______/______/______/______/______/______/_____ /
+        *******************************************************************************
+        ''')
+    else:
+        print('''You tried to open the door. But it stuck. 
+        You pulled as hard as you could and you broke it with part of the wall. 
+        The stones fell down and one of them hit you at the head and killed. 
+        You'd better used your helmet...''')
+        print("")
+        print("Game Over")
+
+
+
+
+
+########################################################################
