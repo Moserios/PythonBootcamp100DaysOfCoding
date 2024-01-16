f648f1d9b166f0a96327c7636b75c90aa83c35c2
Moser <moserturbo@gmail.com>
2024-01-16 18:24:27 +0100

    Day 63. Added Editin rating and removing Books from Library.

--- day63_databases/main.py
+++ day63_databases/main.py
@@ -45,6 +45,27 @@ def add():
         return redirect(url_for('home'))
     return render_template("add.html")
 
+@app.route("/edit_rating", methods=['GET', 'POST'])
+def edit_rating():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get("id")
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route("/delete", methods=['GET', 'POST'])
+def delete():
+    book_id = request.args.get("id")
+
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
